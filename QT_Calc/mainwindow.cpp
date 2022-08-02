ee433a325f1eacd2459bc06c7cb7f3b24fa2c991
monika-shaw-cctech <109352608+monika-shaw-cctech@users.noreply.github.com>
2022-08-02 13:10:45 +0530

    connected all digits

--- QT_Calc/mainwindow.cpp
+++ QT_Calc/mainwindow.cpp
@@ -1,15 +1,34 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QDebug"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+   connect(ui->pushButton_zero,SIGNAL(pressed()),this,SLOT(digit_pressed()));
+   connect(ui->pushButton_one,SIGNAL(pressed()),this,SLOT(digit_pressed()));
+   connect(ui->pushButton_two,SIGNAL(pressed()),this,SLOT(digit_pressed()));
+   connect(ui->pushButton_three,SIGNAL(pressed()),this,SLOT(digit_pressed()));
+   connect(ui->pushButton_four,SIGNAL(pressed()),this,SLOT(digit_pressed()));
+   connect(ui->pushButton_five,SIGNAL(pressed()),this,SLOT(digit_pressed()));
+   connect(ui->pushButton_six,SIGNAL(pressed()),this,SLOT(digit_pressed()));
+   connect(ui->pushButton_seven,SIGNAL(pressed()),this,SLOT(digit_pressed()));
+   connect(ui->pushButton_eight,SIGNAL(pressed()),this,SLOT(digit_pressed()));
+   connect(ui->pushButton_nine,SIGNAL(pressed()),this,SLOT(digit_pressed()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::digit_pressed()
+{
+    qDebug()<<"presses";
+}
+
 
--- QT_Calc/mainwindow.h
+++ QT_Calc/mainwindow.h
@@ -15,6 +15,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void digit_pressed();
+
 private:
     Ui::MainWindow *ui;
 };

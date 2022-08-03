5beb8ed7a9b49af9c022fb19cf9aecb91052312a
monika-shaw-cctech <109352608+monika-shaw-cctech@users.noreply.github.com>
2022-08-03 12:03:58 +0530

    Revert "added the precision"
    This reverts commit 75f4fc97cf837cf78fa9ceee45e405b0c88d6593.

--- QT_Calc/mainwindow.cpp
+++ QT_Calc/mainwindow.cpp
@@ -31,9 +31,6 @@ void MainWindow::digit_pressed()
 //    qDebug()<<"presses";
     QPushButton *button = (QPushButton*)sender();
 
-    double label;
-    QString result;
-
     if(ui->label_2->text() == '0')
     {
          ui->label_2->setText(button->text());
@@ -41,10 +38,7 @@ void MainWindow::digit_pressed()
 
     else
     {
-//        ui->label_2->setText(ui->label_2->text()+button->text());
-          label = (ui->label_2->text()+button->text()).toDouble();
-          result = QString::number(label,'g',15);
-          ui->label_2->setText(result);
+        ui->label_2->setText(ui->label_2->text()+button->text());
     }
 
 }
